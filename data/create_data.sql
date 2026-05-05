5243f27e4411baea20c8003804989fb8d302c9d7
mackenzierossiter <rossitermackenzie@gmail.com>
2026-05-04 23:40:30 -0400

    updated create_data file to have my data

--- data/create_data.sql
+++ data/create_data.sql
@@ -361,3 +361,89 @@ VALUES (77005, 5, 103, 77005, '2026-06-20 14:00:00', '2026-06-22 11:00:00');
 
 INSERT INTO bill(id, total_price, reservation_id, guest_pid)
 VALUES (11003, 890, 88006, 77005);
+
+
+
+-- kenzie query 2
+-- HW 8 query2 - Kenzie
+
+-- all delete queries are used so I can run this file multiple times without errors
+
+-- adding a guest named Mrs. Smith
+
+INSERT INTO guest(pid, name, id_number, id_type, address, mobile_phone, home_phone)
+VALUES (23450, 'Mrs. Smith', 45678, 'liscense', '7 Sweet Lane HBurg, VA', '3479473284', '589095483');
+
+
+-- adding an occupant named Mr. Smith
+
+INSERT INTO occupant(pid, name, is_adult)
+VALUES (65372, 'Mr. Smith', TRUE);
+
+
+-- Add in a hotel named 'Hotel B'
+INSERT INTO hotel(id, name, address)
+VALUES (89604, 'Hotel B', '84 Fun Rd Miami, Fl');
+
+
+-- create Mr. and Mrs. Smith's reservation
+
+INSERT INTO reservations(id, room_type_name, start_date, end_date)
+VALUES (85930, 'Double', '04-21-2026', '04-25-2026');
+
+
+-- Have Mrs. Smith make the reservation
+
+INSERT INTO makes(guest_pid, reservation_id)
+VALUES(23450, 85930);
+
+
+
+-- filling the room_type table
+
+INSERT INTO room_type(id, capacity, name, size)
+VALUES (85204, 2, 'Double', 2);
+
+-- filling in room_type_has_reservation to connect room_type to res
+
+INSERT INTO room_type_has_reservations(room_type_tid, room_type_name, reservations_idreservations)
+VALUES (85204, 'Double', 85930);
+
+
+-- start here
+
+-- empty Double Room 2
+
+INSERT INTO room_type(id, capacity, name, size)
+VALUES (57930, 2, 'Double', 2);
+
+
+-- create empty rooms
+
+INSERT INTO rooms(room_number, hotel_id, room_type_id, floor)
+VALUES(491, 89604, 85204, 4);
+
+INSERT INTO rooms(room_number, hotel_id, room_type_id, floor)
+VALUES(103, 89604, 57930, 1);
+
+
+
+-- Create a filled double room
+-- filled room type
+
+INSERT INTO room_type(id, capacity, name, size)
+VALUES (48024, 2, 'Double', 2);
+
+-- filled rooms
+
+INSERT INTO rooms(room_number, hotel_id, room_type_id, floor)
+VALUES(284, 89604, 48024, 2);
+
+
+INSERT INTO reservations(id, room_type_name, start_date, end_date)
+VALUES (42940, 'Double', '2026-04-21', '2026-04-25');
+
+-- filled reservation_rooms
+
+INSERT into reservations_rooms(reservation_id, hotel_id, room_number)
+VALUES(42940, 89604, 284);
